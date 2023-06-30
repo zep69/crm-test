a787843a9f2f381df1c34578f27a8f90bbf1f4cf
devmegahand <dev@mhand.ru>
2023-06-30 17:28:26 +0400

    add Messages in components2

---
+++ src/components/vueElementChatGpt.vue
@@ -0,0 +1,30 @@
+<template>
+	<button @click="handleClick" class="my-button">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+	name: "vueElementChatGpt",
+	data() {
+		return {
+			buttonText: "Нажми меня",
+		};
+	},
+	methods: {
+		handleClick() {
+			alert("Кнопка была нажата!");
+		},
+	},
+}
+</script>
+
+<style scoped>
+.my-button {
+	padding: 10px 20px;
+	background-color: #3498db;
+	color: #fff;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+}
+</style>
---
+++ src/components/vueGptTAble.vue
@@ -0,0 +1,63 @@
+<template>
+	<table class="my-table">
+		<thead>
+		<tr>
+			<th>Заголовок 1</th>
+			<th>Заголовок 2</th>
+			<th>Заголовок 3</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr v-for="(item, index) in tableData" :key="index">
+			<td>{{ item.column1 }}</td>
+			<td>{{ item.column2 }}</td>
+			<td>{{ item.column3 }}</td>
+		</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+export default {
+	name:'vueGptTable',
+	data() {
+		return {
+			tableData: [
+				{ column1: "Значение 1", column2: "Значение 2", column3: "Значение 3" },
+				{ column1: "Значение 4", column2: "Значение 5", column3: "Значение 6" },
+				{ column1: "Значение 7", column2: "Значение 8", column3: "Значение 9" },
+			],
+		};
+	},
+};
+</script>
+
+<style>
+.my-table {
+	width: 100%;
+	border-collapse: collapse;
+}
+
+.my-table th,
+.my-table td {
+	padding: 10px;
+	border: 1px solid #ccc;
+}
+
+.my-table th {
+	background-color: #f2f2f2;
+	font-weight: bold;
+}
+
+.my-table tbody tr:nth-child(even) {
+	background-color: #f9f9f9;
+}
+
+.my-table tbody tr:hover {
+	background-color: #ebebeb;
+}
+
+.my-table td {
+	text-align: center;
+}
+</style>
